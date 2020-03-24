16b881113806a9ee79d89b725b7f9495e5e3b443
Sui.Young <suisauce.codes@gmail.com>
2020-03-24 16:53:26 -0400

    fixed animation.

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -20,14 +20,17 @@ $('a[href*="#"]').on("click", function(e) {
 $(document).ready(function() {
     $(window).scroll(function() {
         $(".fadeIn").each(function(i) {
-        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        let bottom_of_element = $(this).offset().top + $(this).outerHeight();
+        let bottom_of_window = $(window).scrollTop() + $(window).height();
         if (bottom_of_window > bottom_of_element) {
             $(this).animate({ opacity: "1" }, 500);
         }
         });
     });
 });
+
+
+
 
 // my typed.js customization
 $(function() {
