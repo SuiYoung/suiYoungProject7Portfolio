e1471bdd1f0aa982378d0c9e122877e7cab67a9b
Sui.Young <suisauce.codes@gmail.com>
2020-03-31 20:08:59 -0400

    added toggle and mouse tooltip for name symbol in header

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -3,6 +3,32 @@
 //     console.log('ready');
 // });
 
+// name symbol toggle
+$(".suiButton").on("click", function() {
+    $(".WhatsItMean").fadeToggle();
+});
+
+// Tooltip only Text
+        // Tooltip only Text
+        $('.suiButton2').hover(function(){
+                // Hover over code
+                var title = $(this).attr('title');
+                $(this).data('tipText', title).removeAttr('title');
+                $('<p class="tooltip"></p>')
+                .text(title)
+                .appendTo('body')
+                .fadeIn('slow');
+        }, function() {
+                // Hover out code
+                $(this).attr('title', $(this).data('tipText'));
+                $('.tooltip').remove();
+        }).mousemove(function(e) {
+                var mousex = e.pageX + 20; //Get X coordinates
+                var mousey = e.pageY + 10; //Get Y coordinates
+                $('.tooltip')
+                .css({ top: mousey, left: mousex })
+        });
+
 // smooth scroll
 $('a[href*="#"]').on("click", function(e) {
     e.preventDefault();
